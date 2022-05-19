38e5122c9359fceef47b5eac696cc7801a09af02
mowuj <mowuj92@gmail.com>
2022-05-20 00:47:40 +0600

    add testimonials and footer

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Dental from '../Dental/Dental';
+import Footer from '../Footer/Footer';
 import Info from '../Info/Info';
 import MakeAppoinment from '../MakeAppoinment/MakeAppoinment';
 import Services from '../Services/Services';
+import Testimonials from '../Testimonials/Testimonials';
 
 const Home = () => {
     return (
@@ -13,6 +15,8 @@ const Home = () => {
             <Services></Services>
             <Dental></Dental>
             <MakeAppoinment></MakeAppoinment>
+            <Testimonials></Testimonials>
+            <Footer></Footer>
         </div>
     );
 };
