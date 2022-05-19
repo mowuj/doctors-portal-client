e1aed96404472c462043e7f2cfd007bc26bbbdbf
mowuj <mowuj92@gmail.com>
2022-05-19 23:42:26 +0600

    dental section add

--- src/Pages/Home/Info/Info.js
+++ src/Pages/Home/Info/Info.js
@@ -9,7 +9,7 @@ const Info = () => {
     return (
         <div className='grid grid-clos-1 lg:grid-cols-3 gap-5'>
             <InfoCard cardTitle="Opening Hours"bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
-            <InfoCard cardTitle="Our Locations"bgClass="bg-accent" img={marker}></InfoCard>
+            <InfoCard cardTitle="Our Locations"bgClass="bg-gradient-to-r from-accent to-primary" img={marker}></InfoCard>
             <InfoCard cardTitle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
         </div>
     );
