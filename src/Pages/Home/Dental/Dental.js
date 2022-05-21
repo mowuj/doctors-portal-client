bf9f1c70f865b8756dd7e328092d2e4bde6b181d
mowuj <mowuj92@gmail.com>
2022-05-21 22:36:33 +0600

    change all class to className

--- src/Pages/Home/Dental/Dental.js
+++ src/Pages/Home/Dental/Dental.js
@@ -3,13 +3,13 @@ import treatment from '../../../assets/images/treatment.png'
 const Dental = () => {
     return (
         <div>
-            <div class="hero min-h-screen">
-  <div class="hero-content flex-col lg:flex-row">
-    <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+            <div className="hero min-h-screen">
+  <div className="hero-content flex-col lg:flex-row">
+    <img src={treatment} className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button class="btn btn-primary">Get Started</button>
+      <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
