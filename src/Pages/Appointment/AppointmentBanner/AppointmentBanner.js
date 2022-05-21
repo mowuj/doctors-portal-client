c45e59ece06ad52f494dc2d24ecddc19d4ba118d
mowuj <mowuj92@gmail.com>
2022-05-21 23:45:40 +0600

    add available appointment compo and lift state

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,10 +1,24 @@
-import React from 'react';
 
-const AppointmentBanner = () => {
+import chair from '../../../assets/images/chair.png'
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+const AppointmentBanner = ({date,setDate}) => {
+    
     return (
-        <div>
+        <div class="hero min-h-screen ">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='Dentiest chair' />
+    <div>
+        <DayPicker
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+                    />
+                    
+    </div>
+            </div>
             
-        </div>
+</div>
     );
 };
 
