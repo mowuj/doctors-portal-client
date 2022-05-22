778e0e33119e2c134b8242c8f4711cb76bba9c8f
mowuj <mowuj92@gmail.com>
2022-05-22 22:51:39 +0600

    design login form

--- src/Pages/Home/Testimonials/Testimonials.js
+++ src/Pages/Home/Testimonials/Testimonials.js
@@ -45,7 +45,7 @@ const Testimonials = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
                     reviews.map(review => <Review
-                        key={review.id}
+                        key={review._id}
                         review={review}
                     ></Review>)
                 }
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,18 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_API_KEY,
-  authDomain:process.env.REACT_APP_AUTH_DOMAIN ,
-  projectId:process.env.REACT_APP_PROJECT_ID ,
-  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId:process.env.REACT_APP_APP_ID ,
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyDF63v6YVOvs7bE9A9j02oPod9P9tb71RM",
+  authDomain: "doctors-portal-85d6d.firebaseapp.com",
+  projectId: "doctors-portal-85d6d",
+  storageBucket: "doctors-portal-85d6d.appspot.com",
+  messagingSenderId: "1002540960039",
+  appId: "1:1002540960039:web:865315c1f02687de43c102"
 };
 
 // Initialize Firebase
