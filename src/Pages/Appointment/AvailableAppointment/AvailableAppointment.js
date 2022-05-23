d5ae72cdc03eb948438dbdbdca2c29b935b8f092
mowuj <mowuj92@gmail.com>
2022-05-23 10:45:47 +0600

    update automatic by react query

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,20 +1,25 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import BookingModal from '../BookingModal/BookingModal';
 import Service from '../Service/Service';
-
+import Loading from '../../Shared/Loading/Loading';
 
 const AvailableAppointment = ({ date }) => {
     
-    const [services,setServices] =useState([])
+    // const [services,setServices] =useState([])
     const [treatment, setTreatment] = useState(null)
     const formattedDate = format(date, 'PP')
-    
-    useEffect(() => {
-        fetch(`http://localhost:5000/available?date=${formattedDate}`)
-        .then(res=>res.json())
-        .then(data=>setServices(data))
-    },[formattedDate])
+    const {data:services,isLoading,refetch}=useQuery(['available',formattedDate],()=>fetch(`http://localhost:5000/available?date=${formattedDate}`)
+        .then(res => res.json()))
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+    // useEffect(() => {
+    //     fetch(`http://localhost:5000/available?date=${formattedDate}`)
+    //     .then(res=>res.json())
+    //     .then(data=>setServices(data))
+    // },[formattedDate])
     return (
         <div>
             <h4 className='text-xl text-secondary text-center my-12'>Available Appointments on {format(date, 'PP')}</h4>
@@ -31,6 +36,7 @@ const AvailableAppointment = ({ date }) => {
                 date={date}
                 treatment={treatment}
                 setTreatment={setTreatment}
+                refetch={refetch}
             ></BookingModal>}
         </div>
     );
