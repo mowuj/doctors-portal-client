74b4ab6b169f822bdcc2bfdaac8f697b39c1d5a2
mowuj <mowuj92@gmail.com>
2022-05-25 21:21:30 +0600

    payment stripe

--- src/Pages/Dashboard/CheckoutForm/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm/CheckoutForm.js
@@ -1,10 +1,30 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({appointment}) => {
     const stripe = useStripe();
     const elements = useElements();
-    const [cardError,setCardError]=useState('')
+  const [cardError, setCardError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [transactionId, setTransactionId] = useState('');
+  const [clientSecret, setClientSecret] = useState('');
+  const {price,patient,patientName } = appointment;
+  useEffect(() => {
+    fetch('http://localhost:5000/create-payment-intent', {
+      method: 'POST',
+      headers: {
+        'content-type':'application/json',
+        'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+      },
+      body:JSON.stringify({price})
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data?.clientSecret) {
+          setClientSecret(data.clientSecret);
+        }
+      })
+},[price])
     const handleSubmit = async(event) => {
         event.preventDefault();
         if (!stripe || !elements) {
@@ -20,7 +40,30 @@ const CheckoutForm = () => {
         });
         
             setCardError(error?.message||'');
-       
+      setSuccess('');
+      //confirm card payment
+      const {paymentIntent, error:intentError} = await stripe.confirmCardPayment(
+  clientSecret,
+  {
+    payment_method: {
+      card: card,
+      billing_details: {
+        name: patientName,
+        email:patient
+      },
+    },
+  },
+      );
+      if (intentError) {
+        setCardError(intentError?.message);
+        setSuccess('')
+      }
+      else {
+        setCardError('');
+        setTransactionId(paymentIntent.id)
+        console.log(paymentIntent);
+        setSuccess('Congrats ! Your payment is completed')
+      }
     }
     return (
         <>
@@ -41,12 +84,18 @@ const CheckoutForm = () => {
           },
         }}
       />
-      <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
+      <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe ||!clientSecret}>
         Pay
       </button>
             </form>
             {
                 cardError && <p className='text-red-500'>{ cardError}</p>
+            }
+            {
+          success && <div className='text-green-500'>
+            <p>{success}</p>
+            <p>Your transaction Id: <span className='text-orange-500 font-bold'>{ transactionId}</span></p>
+                </div>
             }
         </>
     );
--- src/Pages/Dashboard/CheckoutForm/__payment.steps.js
+++ src/Pages/Dashboard/CheckoutForm/__payment.steps.js
@@ -4,7 +4,8 @@
 3.get publishable key pk---
 4.Create Elements wrapper using publishable key
 5.Create Checkout Form using Card element,useStripe,useElements
-6.Get card elements info(credit card info)
+6.Get card elements info(credit card info)+display card error(if any)
 -------------------------------
-7.
+7.get client secret form backend via payment intent post api
+8.store client secret on the client side
  */
