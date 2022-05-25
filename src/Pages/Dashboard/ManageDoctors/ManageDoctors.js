315556b0799c678b8d3b76e67685158835aa228a
mowuj <mowuj92@gmail.com>
2022-05-25 11:36:31 +0600

    load all doctor info

--- src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,9 +1,44 @@
-import React from 'react';
 
+import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading/Loading';
+import DoctorRow from '../../Dashboard/DoctorRow/DoctorRow'
 const ManageDoctors = () => {
+    const { data: doctors, isLoading } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
+        headers: {
+            authorization:`Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h2 className="text-2xl">Manage Doctors</h2>
+            <h2 className="text-2xl">Manage Doctors:{ doctors.length}</h2>
+            <div class="overflow-x-auto">
+  <table class="table w-full">
+    
+    <thead>
+      <tr>
+        <th></th>
+        
+        <th>Avatar</th>
+        <th>Name</th>
+        <th>Specialty</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow
+                                key={doctor._key}
+                                doctor={doctor}
+                                index={index}
+                            ></DoctorRow>)                
+        }
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
